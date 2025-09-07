dd5ab38f6a5fd8b1eb42fa2b96c5e7d986a696dc
Markus Wendorf <markuswendorf@web.de>
2025-09-07 17:55:18 +0200

    replace mmap with bufreader

--- src/main.rs
+++ src/main.rs
@@ -6,63 +6,59 @@ mod processing;
 
 use std::collections::{BTreeMap, HashMap, HashSet};
 use std::fs::OpenOptions;
-use std::io::{self, Write};
-
-use std::thread;
-
-use memmap2::MmapMut;
+use std::io::{self, BufRead, BufReader, Read, Seek, SeekFrom, Write};
+use std::thread::{self, available_parallelism};
 
 use crate::data::Data;
 use crate::processing::process_chunk;
 
 fn main() -> io::Result<()> {
     let file = OpenOptions::new().read(true).write(true).open("data.txt")?;
+    let mut reader = BufReader::new(file);
 
-    let mut mmap = unsafe { MmapMut::map_mut(&file)? };
-    mmap.advise(memmap2::Advice::Sequential).unwrap();
+    let total_length = reader.get_ref().metadata()?.len() as usize;
 
-    let chunk_count = 10;
-    let mut indices: [std::ops::Range<usize>; 10] = Default::default();
+    let chunk_count = available_parallelism().unwrap().get() * 2;
+    let chunk_size = total_length / chunk_count;
 
-    let total_length = mmap.len();
-
-    let chunk_size = (total_length / chunk_count) as usize;
-
+    let mut indices: Vec<std::ops::Range<usize>> = Vec::new();
     let mut offset: usize = 0;
 
-    for i in 0..chunk_count {
+    for _ in 0..chunk_count {
         let chunk_start = offset;
         let mut chunk_end = offset + chunk_size;
 
-        let mut chunk_end_pos = chunk_end;
-        loop {
-            match mmap.get(chunk_end_pos) {
-                Some(character) if *character == b'\n' => {
-                    chunk_end = chunk_end_pos;
-                    break;
-                }
-                Some(_) => chunk_end_pos += 1,
-                None => {
-                    chunk_end = total_length;
-                    break;
-                }
-            }
+        if chunk_end >= total_length {
+            chunk_end = total_length;
+        } else {
+            reader.seek(SeekFrom::Start(chunk_end as u64))?;
+            let mut buffer = Vec::new();
+
+            chunk_end = match reader.read_until(b'\n', &mut buffer) {
+                Ok(_) => reader.stream_position()? as usize,
+                _ => total_length,
+            };
         }
 
-        indices[i] = chunk_start..chunk_end;
-        offset = chunk_end + 1;
+        indices.push(chunk_start..chunk_end);
+        offset = chunk_end;
     }
 
     thread::scope(|scope| {
-        let chunks = mmap.get_disjoint_mut(indices).unwrap();
+        let parts = indices
+            .into_iter()
+            .map(|range| {
+                scope.spawn(move || {
+                    let file = OpenOptions::new().read(true).open("data.txt").unwrap();
+                    let mut reader = BufReader::new(file);
+                    reader.seek(SeekFrom::Start(range.start as u64)).unwrap();
 
-        let parts = chunks
-            .map(|chunk| {
-                scope.spawn(|| {
-                    let data = process_chunk(chunk);
+                    let mut buffer = vec![0u8; range.len()];
+                    reader.read_exact(&mut buffer).unwrap();
+
+                    let data = process_chunk(&buffer);
 
                     let mut part: HashMap<String, Data> = HashMap::new();
-
                     for (station, temperatures) in data {
                         let mut data = Data::default();
                         data.station = String::from_utf8_lossy(station).into_owned();
@@ -70,20 +66,17 @@ fn main() -> io::Result<()> {
                         for temperature in temperatures {
                             data.update(temperature);
                         }
-
                         part.insert(data.station.clone(), data);
                     }
-
                     part
                 })
             })
-            .into_iter();
+            .collect::<Vec<_>>();
 
         let processed_parts: Vec<HashMap<String, Data>> =
-            parts.map(|p| p.join().unwrap()).collect();
+            parts.into_iter().map(|p| p.join().unwrap()).collect();
 
         let mut station_keys: HashSet<&String> = HashSet::new();
-
         for part in processed_parts.iter() {
             for key in part.keys() {
                 station_keys.insert(key);
@@ -91,44 +84,39 @@ fn main() -> io::Result<()> {
         }
 
         let mut combined: BTreeMap<String, Data> = BTreeMap::new();
-
         for part in processed_parts.iter() {
-            for j in station_keys.iter() {
-                let key = *j;
-                if let Some(data) = part.get(key) {
+            for key in station_keys.iter() {
+                if let Some(data) = part.get(*key) {
                     combined
-                        .entry(key.clone())
+                        .entry((*key).clone())
                         .and_modify(|d| d.merge(data))
                         .or_insert(data.clone());
                 }
             }
         }
 
-        {
-            let mut stdout = std::io::stdout().lock();
-            stdout.write(b"{").unwrap();
+        let mut stdout = std::io::stdout().lock();
+        stdout.write_all(b"{").unwrap();
 
-            let last_index = combined.len() - 1;
-            for (i, (station, data)) in combined.iter().enumerate() {
-                let mean = data.total as f32 / data.count as f32;
+        let last_index = combined.len() - 1;
+        for (i, (station, data)) in combined.iter().enumerate() {
+            let mean = data.total as f32 / data.count as f32;
+            let line = format!(
+                "{}={}/{}/{}",
+                station,
+                format_number(data.min),
+                format_mean(mean),
+                format_number(data.max)
+            );
 
-                let line = station.to_owned()
-                    + "="
-                    + format_number(data.min).as_str()
-                    + "/"
-                    + format_mean(mean).as_str()
-                    + "/"
-                    + format_number(data.max).as_str();
+            stdout.write_all(line.as_bytes()).unwrap();
 
-                stdout.write(&line.as_bytes()).unwrap();
-
-                if i != last_index {
-                    stdout.write(b", ").unwrap();
-                }
+            if i != last_index {
+                stdout.write_all(b", ").unwrap();
             }
-
-            stdout.write(b"}").unwrap();
         }
+
+        stdout.write_all(b"}").unwrap();
     });
 
     Ok(())
